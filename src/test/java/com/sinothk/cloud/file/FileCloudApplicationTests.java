bf2081807aae7c75f858b47955465699917af046
LiangYT <381518188@qq.com>
2019-09-13 22:48:19 +0800

    初始化！

---
+++ src/test/java/com/sinothk/cloud/file/FileCloudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sinothk.cloud.file;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileCloudApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
