6019183b868519ad306f4a904d45890fe8bef314
LiangYT <381518188@qq.com>
2019-09-16 23:14:39 +0800

    完成文件服务基本功能

--- src/main/java/com/sinothk/cloud/FileCloudApplication.java
+++ src/main/java/com/sinothk/cloud/FileCloudApplication.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.spring.web.SpringfoxWebMvcConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -11,9 +13,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2 // api
 @MapperScan("com.sinothk.cloud.file.repository")
 @ConditionalOnClass(SpringfoxWebMvcConfiguration.class) // https://doc.xiaominfo.com/guide/useful.html#注意事项
-public class FileCloudApplication {
+public class FileCloudApplication  implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(FileCloudApplication.class, args);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
