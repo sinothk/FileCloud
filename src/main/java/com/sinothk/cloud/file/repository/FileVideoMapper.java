44f17d69fb78c34b82bb440ac036f27b43004c2b
LiangYT <381518188@qq.com>
2019-09-30 16:35:16 +0800

    完成文件服务基本功能

--- src/main/java/com/sinothk/cloud/file/repository/FileVideoMapper.java
+++ src/main/java/com/sinothk/cloud/file/repository/FileVideoMapper.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Repository;
 public interface FileVideoMapper extends BaseMapper<FileVideoEntity> {
 
     @Select("SELECT a.* FROM tb_file_video AS a " +
-            "RIGHT JOIN (SELECT biz_id, MAX(create_time) AS maxtime FROM tb_file_video WHERE owner_user = \"${vo.getOwnerUser()}\" AND " +
+            "RIGHT JOIN (SELECT biz_id, MAX(create_time) AS maxtime FROM tb_file_video WHERE owner_user = \"${vo.getOwnerUser()}\" AND file_type = \"${vo.getFileType()}\" AND" +
             " biz_id IS NOT NULL GROUP BY biz_id) AS b ON a.biz_id=b.biz_id AND a.create_time=b.maxtime ORDER BY a.create_time DESC")
     IPage<FileVideoEntity> selectFilePageList(IPage page, @Param("vo") FileVo vo);
 }
