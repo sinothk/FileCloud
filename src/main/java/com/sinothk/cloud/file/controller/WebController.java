308c9f1dcfd7226cd34ae11ffbf4215592ca8b32
LiangYT <381518188@qq.com>
2019-12-19 17:26:17 +0800

    完成文件服务基本功能

--- src/main/java/com/sinothk/cloud/file/controller/WebController.java
+++ src/main/java/com/sinothk/cloud/file/controller/WebController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebController {
 
-    @RequestMapping("/home")
+    @RequestMapping("/index.stk")
     public String test() {
         return "index";
     }
