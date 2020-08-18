9e0a7be43d52b6bdf439e9db9c2ea4d83b1dc8f6
LiangYT <381518188@qq.com>
2020-08-18 14:33:51 +0800

    完成文件服务基本功能

--- src/test/java/com/sinothk/cloud/file/TokenUtilTest.java
+++ src/test/java/com/sinothk/cloud/file/TokenUtilTest.java
@@ -4,28 +4,28 @@ import com.sinothk.base.utils.TokenUtil;
 
 public class TokenUtilTest {
 
-    public static void main(String[] args) {
-
-        String token = TokenUtil.createToken(TokenUtil.EXPIRE_TIME_15D, "admin");
-
-//        HashMap<String, Object> map = new HashMap<>();
-//        map.put("userAccount", 10000);
-//        map.put("userName", "liangyt");
-//        map.put("userRoles", new String[]{"Admin", "MANAGER"});
+//    public static void main(String[] args) {
 //
-//        String token = TokenUtil.createToken(TokenUtil.EXPIRE_TIME_15D, map);
-
-        System.out.println("token = " + token);
-
-//        String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnY2xhc3MiLCJ1c2VyTmFtZSI6ImxpYW5neXQiLCJleHAiOjE1Njg0Nzg2MTl9.Lpw7SoGUo7xZ0GoHfxubyvyReUdvk8osqDszF5mzB7Q";
-//        Claims claims = TokenUtil.checkToken(token);
+//        String token = TokenUtil.createToken(TokenUtil.EXPIRE_TIME_15D, "admin");
 //
-//        if (claims == null) {
-//            // 错误token，超时都返回null
-//            System.out.println("验证失败");
-//        } else {
-//            System.out.println("验证通过");
-//            System.out.println("userName = " + TokenUtil.getUserName(token));
-//        }
-    }
+////        HashMap<String, Object> map = new HashMap<>();
+////        map.put("userAccount", 10000);
+////        map.put("userName", "liangyt");
+////        map.put("userRoles", new String[]{"Admin", "MANAGER"});
+////
+////        String token = TokenUtil.createToken(TokenUtil.EXPIRE_TIME_15D, map);
+//
+//        System.out.println("token = " + token);
+//
+////        String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnY2xhc3MiLCJ1c2VyTmFtZSI6ImxpYW5neXQiLCJleHAiOjE1Njg0Nzg2MTl9.Lpw7SoGUo7xZ0GoHfxubyvyReUdvk8osqDszF5mzB7Q";
+////        Claims claims = TokenUtil.checkToken(token);
+////
+////        if (claims == null) {
+////            // 错误token，超时都返回null
+////            System.out.println("验证失败");
+////        } else {
+////            System.out.println("验证通过");
+////            System.out.println("userName = " + TokenUtil.getUserName(token));
+////        }
+//    }
 }
